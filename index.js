e34462ea1c43a8c38e36fa2bbd6483076bec96bb
Bobby Hirtle <6433499+protonbobby@users.noreply.github.com>
2019-08-05 09:58:34 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+{
+  "0": {
+    "name": "Support",
+    "color": "#397db8",
+    "map": [
+      {
+        "name": "TSI <3 Organization",
+        "url": "https://docs.google.com/spreadsheets/d/1u1lhbCvhwMfi7927tEIrUAuoyhlV-BO7CLKGRgBtXRo/edit?usp=sharing"
+      },
+      {
+        "name": "Training Process",
+        "url": "https://docs.google.com/spreadsheets/d/1UEYK6I-WFMuBk9yQ3fNZh1MaT_CbBSH8YttWy3Y3JY4/edit?usp=sharing"
+      },
+      {
+        "name": "InOut Grid",
+        "url": "https://inoutgrid.buildinglink.com/v2/global/login/login.aspx"
+      }
+    ]
+  },
+  "1": {
+    "name": "FreshDesk",
+    "color": "#28c271",
+    "map": [
+      {
+        "name": "My Tickets",
+        "url": "https://buildinglinksupport.freshdesk.com/a/tickets/filters/259718"
+      },
+      {
+        "name": "Freshconnect",
+        "url": "https://buildinglinksupport.freshconnect.io/conversations"
+      },
+      {
+        "name": "Knowledge Base (Internal)",
+        "url": "https://buildinglinksupport.freshdesk.com/support/home"
+      },
+      {
+        "name": "Live Stats and Figures",
+        "url": "https://share.geckoboard.com/dashboards/L4QD7Z3E7Q5RFI63"
+      },
+      {
+        "name": "Site Status Tracker",
+        "url": "https://updates.freshdesk.com/"
+      },
+      {
+        "name": "Description Mapping Tree",
+        "url": "https://docs.google.com/document/d/1DWlyPCmUYriY0r2gXGbsHJ4CkDIxdQ_IWa3lVYroLwA/edit?usp=sharing"
+      }
+    ]
+  }
+}
