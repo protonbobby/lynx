bdb31a3765b147961cfe53a036a886785685a612
Bobby Hirtle <6433499+protonbobby@users.noreply.github.com>
2019-08-05 10:09:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@
         "url": "https://docs.google.com/spreadsheets/d/1UEYK6I-WFMuBk9yQ3fNZh1MaT_CbBSH8YttWy3Y3JY4/edit?usp=sharing"
       },
       {
-        "name": "InOut Grid",
+        "name": "InOut Grid (BuildingLinkInOut | 12345)",
         "url": "https://inoutgrid.buildinglink.com/v2/global/login/login.aspx"
       }
     ]
@@ -90,6 +90,78 @@
       {
         "name": "Public Display (Display Flip | 12345)",
         "url": "https://publicdisplay.buildinglink.com/v2/global/login/login.aspx"
+      }
+    ]
+  },
+  "3": {
+    "name": "Resources",
+    "color": "green",
+    "map": [
+      {
+        "name": "Submit A Bug Report",
+        "url": "mailto:bugs@buildinglink.com;?cc=training@buildinglink.com%20&subject=Product,%20Module%20%E2%80%93%20Title/Description&body=SUMMARY%0A%0A%0ASTEPS%20TO%20RECREATE%0A1.%0A2.%0A3.%0A%0ARESOLUTION%0A%0A%0AGo%20to%20https%3A//bit.ly/2RwYFjo%20for%20full%20bug%20report%20guide"
+      },
+      {
+        "name": "Submit A Feature Request",
+        "url": "https://docs.google.com/forms/d/e/1FAIpQLSc2AMge6jk2B49LnOnZBqscigrBnrZlnmm7oOc3CrR4uL0H8g/viewform"
+      },
+      {
+        "name": "Look Up A Bug (TFS)",
+        "url": "https://tfs.buildinglink.com/tfs/BuildingLink/Default/Web%20Bugs/_backlogs/board/Stories"
+      },
+      {
+        "name": "Look Up A Bug (Trello)",
+        "url": "https://trello.com/b/FuRdRRWo/legacy-web-bugs"
+      },
+      {
+        "name": "Look Up Existing Requests",
+        "url": "https://docs.google.com/spreadsheets/d/1S3w6pkAFbjOrGXsw_e1JP8NAaD5u0KZ5RMd7a2l24k4/edit#gid=316893508"
+      },
+      {
+        "name": "Resident Help Site",
+        "url": "http://help2.buildinglink.com/home"
+      },
+      {
+        "name": "Employee Help Site",
+        "url": "http://help.buildinglink.com/home"
+      },
+      {
+        "name": "SPF Record Testing",
+        "url": "https://www.kitterman.com/spf/validate.html"
+      },
+      {
+        "name": "Mail Servers",
+        "url": "https://prtg-att.buildinglink.com/public/mapshow.htm?id=3070&mapid=07694E66-B48E-4D4A-9296-C58681BA7E17"
+      }
+    ]
+  },
+  "4": {
+    "name": "Forms",
+    "color": "#397db8",
+    "map": [
+      {
+        "name": "Qualtrics",
+        "url": "https://co1.qualtrics.com/login?path=%2FControlPanel%2F&product=ControlPanel"
+      },
+      {
+        "name": "Sales Call Form",
+        "url": "https://buildinglink.wufoo.com/forms/r1neftg01ev538m/"
+      },
+      {
+        "name": "Equipment Order Form",
+        "url": "https://buildinglink.wufoo.com/forms/buildinglink-equipment-request/"
+      },
+      {
+        "name": "Equipment Order Form (Canada)",
+        "url": "https://buildinglink.wufoo.com/forms/canada-buildinglink-equipment-request/"
+      },
+      {
+        "name": "KeyLink Order Form",
+        "url": "https://buildinglink.wufoo.com/forms/keylink-equipment-request/"
+      },
+      {
+        "name": "Customer Comments Form",
+        "url": "https://buildinglink.com/Public/Feedback.aspx"
       }
     ]
   }
