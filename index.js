6f3d89f0dd6df5efd2d6eb36694b80ada5bcfb8b
Bobby Hirtle <6433499+protonbobby@users.noreply.github.com>
2019-08-13 16:44:57 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,230 @@
+{
+  "0": {
+    "name": "Support",
+    "color": "red",
+    "map": [
+      {
+        "name": "TSI <3 Organization",
+        "url": "https://docs.google.com/spreadsheets/d/1u1lhbCvhwMfi7927tEIrUAuoyhlV-BO7CLKGRgBtXRo/edit?usp=sharing"
+      },
+      {
+        "name": "Training Process",
+        "url": "https://docs.google.com/spreadsheets/d/1UEYK6I-WFMuBk9yQ3fNZh1MaT_CbBSH8YttWy3Y3JY4/edit?usp=sharing"
+      },
+      {
+        "name": "BuildingLink Slack",
+        "url": "https://buildinglink.slack.com/messages/general/"
+      },
+      {
+        "name": "ScreenConnect",
+        "url": "http://connect.buildinglink.com"
+      },
+      {
+        "name": "LogMe",
+        "url": "https://www.logmein.com"
+      },
+      {
+        "name": "InOut Grid",
+        "url": "https://inoutgrid.buildinglink.com/v2/global/login/login.aspx"
+      },
+      {
+      "name" : "Manage Users",
+      "url" : "www.buildinglink.com/V2/Admin/Users/Search.aspx"
+      },
+      {
+      "name" : "Manage SuperUsers",
+      "url" : "www.buildinglink.com/v2/Admin/Superuser/Default.aspx"
+      }
+    ]
+  },
+  "1": {
+    "name": "FreshDesk",
+    "color": "green",
+    "map": [
+      {
+        "name": "My Tickets",
+        "url": "https://buildinglinksupport.freshdesk.com/a/tickets/filters/259718"
+      },
+      {
+        "name": "Freshconnect",
+        "url": "https://buildinglinksupport.freshconnect.io/conversations"
+      },
+      {
+        "name": "Knowledge Base (Internal)",
+        "url": "https://buildinglinksupport.freshdesk.com/support/home"
+      },
+      {
+        "name": "Live Stats and Figures",
+        "url": "https://share.geckoboard.com/dashboards/L4QD7Z3E7Q5RFI63"
+      },
+      {
+        "name": "Site Status Tracker",
+        "url": "https://updates.freshdesk.com/"
+      },
+      {
+        "name": "Description Mapping Tree",
+        "url": "https://docs.google.com/document/d/1DWlyPCmUYriY0r2gXGbsHJ4CkDIxdQ_IWa3lVYroLwA/edit?usp=sharing"
+      }
+    ]
+  },
+  "2": {
+    "name": "Quick Logins",
+    "color": "black",
+    "map": [
+      {
+        "name": "1st Lev Occupant",
+        "url": "https://owner.buildinglink.com/V2/Tenant/Home/DefaultNew.aspx"
+      },
+      {
+        "name": "2nd Lev Occupant",
+        "url": "https://renter.buildinglink.com/V2/Tenant/Home/DefaultNew.aspx"
+      },
+      {
+        "name": "Board Member",
+        "url": "https://board.buildinglink.com/V2/Tenant/Home/DefaultNew.aspx"
+      },
+      {
+        "name": "Security Officer",
+        "url": "https://securityofficer.buildinglink.com/V2/mgmt/default.aspx"
+      },
+      {
+        "name": "Manager",
+        "url": "https://manager.buildinglink.com/v2/mgmt/default.aspx"
+      },
+      {
+        "name": "Maintenance",
+        "url": "https://maintenance.buildinglink.com/v2/mgmt/default.aspx"
+      },
+      {
+        "name": "Front Desk",
+        "url": "https://frontdesk.buildinglink.com/v2/mgmt/default.aspx"
+      },
+      {
+        "name": "Valet",
+        "url": "https://valet.buildinglink.com/v2/mgmt/default.aspx"
+      },
+      {
+        "name": "Superuser",
+        "url": "https://superuser.buildinglink.com/v2/superuser/defaultframe.aspx"
+      },
+      {
+        "name": "Public Display",
+        "url": "https://publicdisplay.buildinglink.com/v2/global/login/login.aspx"
+      },
+      {
+        "name": "(passwords)",
+        "url": "https://docs.google.com/document/d/19f97K7HcVtPU_tP8oHt9Us4PHES1S4Q3DN4T4jNncHE/edit?usp=sharing"
+      }
+    ]
+  },
+  "3": {
+    "name": "Resources",
+    "color": "black",
+    "map": [
+      {
+        "name": "Submit A Bug Report",
+        "url": "mailto:bugs@buildinglink.com;?cc=training@buildinglink.com%20&subject=Product,%20Module%20%E2%80%93%20Title/Description&body=SUMMARY%0A%0A%0ASTEPS%20TO%20RECREATE%0A1.%0A2.%0A3.%0A%0ARESOLUTION%0A%0A%0AGo%20to%20https%3A//bit.ly/2RwYFjo%20for%20full%20bug%20report%20guide"
+      },
+      {
+        "name": "Submit A Feature Request",
+        "url": "https://docs.google.com/forms/d/e/1FAIpQLSc2AMge6jk2B49LnOnZBqscigrBnrZlnmm7oOc3CrR4uL0H8g/viewform"
+      },
+      {
+        "name": "Look Up A Bug (TFS)",
+        "url": "https://tfs.buildinglink.com/tfs/BuildingLink/Default/Web%20Bugs/_backlogs/board/Stories"
+      },
+      {
+        "name": "Look Up A Bug (Trello)",
+        "url": "https://trello.com/b/FuRdRRWo/legacy-web-bugs"
+      },
+      {
+        "name": "Look Up Existing Requests",
+        "url": "https://docs.google.com/spreadsheets/d/1S3w6pkAFbjOrGXsw_e1JP8NAaD5u0KZ5RMd7a2l24k4/edit#gid=316893508"
+      },
+      {
+        "name": "Resident Help Site",
+        "url": "http://help2.buildinglink.com/home"
+      },
+      {
+        "name": "Employee Help Site",
+        "url": "http://help.buildinglink.com/home"
+      },
+      {
+        "name": "SPF Record Testing",
+        "url": "https://www.kitterman.com/spf/validate.html"
+      },
+      {
+        "name": "Mail Servers",
+        "url": "https://prtg-att.buildinglink.com/public/mapshow.htm?id=3070&mapid=07694E66-B48E-4D4A-9296-C58681BA7E17"
+      },
+      {
+        "name": "Outlook 365",
+        "url": "https://outlook.office365.com/mail/inbox"
+      },
+      {
+        "name": "Bamboo HR",
+        "url": "https://buildinglink.bamboohr.com/login.php"
+      }
+    ]
+  },
+  "4": {
+    "name": "Forms",
+    "color": "#397db8",
+    "map": [
+      {
+        "name": "Qualtrics",
+        "url": "https://co1.qualtrics.com/login?path=%2FControlPanel%2F&product=ControlPanel"
+      },
+      {
+        "name": "Sales Call",
+        "url": "https://buildinglink.wufoo.com/forms/r1neftg01ev538m/"
+      },
+      {
+        "name": "Equipment Order (US)",
+        "url": "https://buildinglink.wufoo.com/forms/buildinglink-equipment-request/"
+      },
+      {
+        "name": "Equipment Order (CAN)",
+        "url": "https://buildinglink.wufoo.com/forms/canada-buildinglink-equipment-request/"
+      },
+      {
+        "name": "KeyLink Order",
+        "url": "https://buildinglink.wufoo.com/forms/keylink-equipment-request/"
+      },
+      {
+        "name": "Customer Comments",
+        "url": "https://buildinglink.com/Public/Feedback.aspx"
+      }
+    ]
+  },
+  "5": {
+    "name": "Company",
+    "color": "#397db8",
+    "map": [
+      {
+        "name": "Staging",
+        "url": "https://staging.buildinglink.com/v2/global/login/login.aspx"
+      },
+      {
+        "name": "Public Site",
+        "url": "http://www.buildinglink.com/marketing/public/main"
+      },
+      {
+        "name": "Status Tracker",
+        "url": "https://www.buildinglinkstatus.com/"
+      },
+      {
+        "name": "Blog",
+        "url": "http://www.buildinglinkblog.com/"
+      },
+      {
+        "name": "Advantage Network",
+        "url": "http://www.buildinglink.com/public/advantage2/default.aspx"
+      },
+      {
+        "name": "API",
+        "url": "https://developer.buildinglink.com/apis"
+      }
+    ]
+  }
+}
