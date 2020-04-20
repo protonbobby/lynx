121106fa070c2210c6c9545a8cc5bab1e0b8b889
Bobby Hirtle <6433499+protonbobby@users.noreply.github.com>
2020-04-20 11:30:04 -0400

    added support links

---
+++ tsi_index.js
@@ -0,0 +1,38 @@
+[
+{
+"section": "Support",
+"links": [
+{
+"href": "https://docs.google.com/spreadsheets/d/1u1lhbCvhwMfi7927tEIrUAuoyhlV-BO7CLKGRgBtXRo/edit?usp=sharing",
+"name": "TSI <3 Organization"
+},
+{
+"href": "https://docs.google.com/spreadsheets/d/1UEYK6I-WFMuBk9yQ3fNZh1MaT_CbBSH8YttWy3Y3JY4/edit?usp=sharing",
+"name": "Training Process"
+},
+{
+"href": "https://buildinglink.slack.com/messages/general/",
+"name": "BuildingLink Slack"
+},
+{
+"links": [
+{
+"href": "http://connect.buildinglink.com",
+"name": "ConnectWise     "
+,
+"href": "https://www.logmein.com",
+"name": "LogMeIn"
+}
+]
+},
+{
+"name": "Manage Users",
+"href": "https://buildinglink.com/V2/Admin/Users/Search.aspx"
+},
+{
+"name": "Manage SuperUsers",
+"href": "https://buildinglink.com/v2/Admin/Superuser/Default.aspx"
+}
+],
+}
+]
